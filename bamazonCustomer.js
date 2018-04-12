ebf5f09b4e193b9b8c28f47afe41e1779f9fe764
nkand84 <nityabhagi@gmail.com>
2018-04-12 16:53:51 -0700

    psuedo coded customer js file

---
+++ bamazonCustomer.js
@@ -0,0 +1,13 @@
+// npm packages
+
+
+// table should be dsiplayed when the app starts
+// questions are displayed 
+    // "what is the id of the item you would like to purchase? [Quit with Q]"
+    // how many would you like [Quit with Q]? 
+// table is displayed again with the updated stock price of that item
+// show the questions again
+// if the item is out of stock(if user enters more than the given stock qty)
+// message is displayed "Insufficient quantity!!"
+// display table of items again
+// app starts again with prompt of questions 
