6c03f4e486dd52c808405ed94bdce1c29ea0d66f
nkand84 <nityabhagi@gmail.com>
2018-04-12 18:48:28 -0700

    basic logic working except for update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -49,17 +49,48 @@ function askQuestions(res) {
         }
     ])
         .then(function (answer) {
-            // console.log(answer);
+            // console.log(answer.question1);
             // loop through the array of objects
+            var userSelection = answer.question1;
+            var userSelectionQty = answer.question2;
+            console.log(userSelection);
+            console.log(userSelectionQty);
             for (var i = 0; i < res.length; i++) {
-                console.log("id found " + res[i].item_id);
-                if (answer.question1 === res[i].item_id) {
-                    // table is displayed again with the updated stock price of that item
+                // console.log("id found " + res[i].item_id);
+                if (userSelection == res[i].item_id) {
+                    // console.log("you got it ");
+                    // get the stock qty of that particular item
+                    var newStockQty = res[i].stock_quantity - userSelectionQty;
+                    console.log(newStockQty);
+                    // update the table with the new updated value
+                    updateStockValue(userSelection,newStockQty);
                 }
             }
         });
 }
 
+function updateStockValue(userSelection,newStockQty) {
+    console.log("Updating the stock quantity...\n");
+    var query = connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+            {
+                stock_quantity: newStockQty
+            },
+            {
+                item_id: userSelection
+            }
+        ],
+        function (err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + " product stock updated!\n");
+            // logs the actual query being run
+            console.log(query.sql);
+            // reading products again
+            readProducts();
+        }
+    )
+}
 // show the questions again
 // if the item is out of stock(if user enters more than the given stock qty)
 // message is displayed "Insufficient quantity!!"
